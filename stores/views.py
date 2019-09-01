558ecd59df5183163470bce2b1ba38ea22982d33
Manuel Nasif <manuel126519@gmail.com>
2019-09-01 08:44:49 +0100

    Manuel and Mohammad

--- stores/views.py
+++ stores/views.py
@@ -4,10 +4,29 @@ from .forms import StoreModelForm
 
 # Create your views here.
 def store_list(request):
-    context = {
-        "stores": Store.objects.all()
-    }
-    return render(request, 'store_list.html', context)
+	context = {
+		"stores": Store.objects.all()
+	}
+	return render(request, 'store_list.html', context)
+
+def store_detail(request,store_slug):
+	context = {
+	"store":Store.objects.get(slug=store_slug)
+	}
+	return render(request,'detail.html',context)
+
+def store_create(request):
+	form = StoreModelForm()
+	if request.method == "POST":
+		form = StoreModelForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('list')
+	context={
+	"form":form
+	}
+	return render(request,'create.html',context)
+
 
 
 
